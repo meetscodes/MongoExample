1d42e9dde0c24e5eeaf0567fc13e70f698b69f1f
meetscodes <lakhanimeet0098@gmail.com>
2023-07-07 16:44:45 +0530

    welcome to dashboard

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {MongoClient} = require('mongodb');
+
+const { MongoClient } = require('mongodb');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const bcrypt = require( 'bcrypt');
 
 
 // Connection URL and database name
@@ -18,11 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Create a schema for the data
 const dataSchema = new mongoose.Schema({
-  input1: String,
-  input2: String
+  username: String,
+  email: String,
+  password: String
 });
 // Create a model for the data
 const Data = mongoose.model('Data', dataSchema);
+
+ // Login route
+ app.get('/login', (req, res) => {
+  res.redirect('login.html');
+});
 
 
 // Connect to the MongoDB database
@@ -30,34 +38,68 @@ mongoose.connect(`${url}/${dbName}`, { useNewUrlParser: true, useUnifiedTopology
   .then(() => {
     console.log('Connected to the database');
 
-    // Handle POST request
-    app.post('/submit-form', (req, res) => {
-      const { input1, input2 } = req.body;
 
-      // Create a new data document
-      const newData = new Data({
-        input1,
-        input2
-      });
+   
 
-      // Save the data document
-      newData.save()
-        .then(() => {
-          console.log('Data stored successfully');
-          return res.redirect('/success.html');
-        })
-        .catch(err => {
-          console.error('Error storing data', err);
-          return res.redirect('/error.html');
-        });
+    app.post('/submit-form', async (req, res) => {
+      const { username, email, password } = req.body;
+    
+      try {
+        // Check if the username or email is already registered
+        const existingUser = await Data.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+          return res.render('register', { error: 'Username or email already exists' });
+        }
+    
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        // Create a new user
+        const newUser = new Data({ username, email, password: hashedPassword });
+        await newUser.save();
+    
+        return res.redirect('/login.html');
+      } catch (error) {
+        console.error('Failed to register user:', error);
+      return res.redirect('/error.html');
+      }
     });
-
+    
+    // Login route
+    app.get('/login', (req, res) => {
+      res.redirect('/dashboard.html');
+    });
+    
+    app.post('/login', async (req, res) => {
+      const { username, password } = req.body;
+    
+      try {
+        // Find the user by username
+        const user = await Data.findOne({ username });
+        if (!user) {
+          return res.render('login', { error: 'Invalid username or password' });
+        }
+    
+        // Compare the password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          return res.render('login', { error: 'Invalid username or password' });
+        }
+    
+        return res.redirect('/dashboard.html');
+      } catch (error) {
+        console.error('Failed to login:', error);
+        res.render('login', { error: 'Failed to login' });
+      }
+    });
+  
     // Start the server
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
-  })
-  .catch(err => {
-    console.error('Error connecting to the database', err);
-  });
+        app.listen(port, () => {
+          console.log(`Server running on port ${port}`);
+        });
+      },err=>{
+        console.error('Error connecting to the database', err);
 
+      });
+     
+  
